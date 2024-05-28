091a1abcd8eb110aa9d69f1a7d3c2fbeb29306f7
moai <2316799819@qq.con>
2024-05-28 21:53:34 +0800

    stream create

--- JDK8Special/src/main/java/com/modules/pojo/Person.java
+++ JDK8Special/src/main/java/com/modules/pojo/Person.java
@@ -3,6 +3,8 @@ package com.modules.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Objects;
+
 /**
  * @author moai
  */
@@ -19,4 +21,16 @@ public class Person {
 
     private String sex;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name) && Objects.equals(age, person.age) && Objects.equals(sex, person.sex);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, sex);
+    }
 }
--- JDK8Special/src/main/java/com/modules/stream/SerialStream.java
+++ JDK8Special/src/main/java/com/modules/stream/SerialStream.java
@@ -3,18 +3,23 @@ package com.modules.stream;
 import com.modules.pojo.Person;
 import org.junit.Test;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class SerialStream {
 
-    private final static List<Person> PERSON_LIST = List.of(
+    // foreach map filter limit sorted
+    private final List<Person> PERSON_LIST = List.of(
             new Person("Neo", 22, "male"),
             new Person("Stan", 11, "female"),
             new Person("Grace", 34, "female"),
             new Person("Alex", 50, "male"),
-            new Person("Sebastian", 8, "male")
+            new Person("Sebastian", 8, "male"),
+            new Person("Sebastian", 8, "male"),
+            new Person("Grace", 34, "female")
     );
 
     @Test
@@ -22,8 +27,48 @@ public class SerialStream {
         // 所有实现 Collection接口
         Stream<Person> stream = PERSON_LIST.stream();
 
-        
+        Stream.Builder<Person> personBuilder = Stream.builder();
+    }
+
+    @Test
+    public void  filteringSlicing() {
+        // filter distinct limit skip
+        PERSON_LIST.stream()
+                .filter(person -> person.getAge() > 18)
+                .distinct()
+                .limit(5)
+                .skip(1)
+                .forEach(System.out::println);
 
     }
 
+    @Test
+    public void mapping() {
+        // 映射结果
+        Stream<String> nameString = PERSON_LIST.stream().map(Person::getName);
+        nameString.forEach(System.out::println);
+    }
+
+    @Test
+    public void  sorting() {
+        List<Person> personList = List.of(
+                new Person("Neo", 22, "male"),
+                new Person("Stan", 11, "female"),
+                new Person("Grace", 34, "female"),
+                new Person("Alex", 50, "male"),
+                new Person("Sebastian", 8, "male"),
+                new Person("Sebastian", 8, "male"),
+                new Person("Grace", 34, "female")
+        );
+        List<Integer> ageList = personList.stream().map(Person::getAge).collect(Collectors.toList());
+        ageList.stream().sorted().forEach(System.out::println);
+    }
+
+    @Test
+    public void aggregation() {
+        // count max min average sum
+        long count = PERSON_LIST.stream().count();
+        Optional<Person> max = PERSON_LIST.stream().max(Comparator.comparingInt(Person::getAge));
+
+    }
 }
